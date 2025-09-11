99e4515c81d5f945f1b69966eb782b47e27af40d
codewithna <codewithna73@gmail.com>
2025-09-11 16:09:30 +0530

    new folder add

---
+++ react3/react3/src/user.jsx
@@ -0,0 +1,25 @@
+function Login(){
+    return(
+        <div>
+            <h2>Login</h2>
+        </div>
+    )
+}
+
+export function Profiles(){
+  return(
+    <div>
+      <h2>Profhile</h2>
+    </div>
+  )
+}
+export function Seting(){
+  return(
+    <div>
+      <h2>Seting</h2>
+    </div>
+  )
+}
+
+export const userkey = "@nirtJddfd2534"
+export default Login;
