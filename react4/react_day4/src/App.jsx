3e5106064f3889f168a773f035763a57f441c538
codewithna <codewithna73@gmail.com>
2025-09-12 18:45:10 +0530

    new folder add

---
+++ react4/react_day4/src/App.jsx
@@ -0,0 +1,12 @@
+import FristPro from "./FristProjects";
+
+function App() {
+  return(
+   
+  <div>
+     <FristPro></FristPro>
+  </div>
+  )
+}
+
+export default App;
---
+++ react4/react_day4/src/FristProjects.jsx
@@ -0,0 +1,22 @@
+function FristPro() {
+    function almay(){
+        alert("bhut aasan hey react");
+    }
+   
+    return (
+        <>
+        <div>
+            <h1>Wellcome to my web 30+20 = {30+20}</h1>
+            <ul>
+                <li>Nitesh home</li>
+                <li>Is a vey nice</li>
+                <li>Pllis com to may home</li>
+            </ul>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlnFm21AAigoxWw7bxJc7zW9kM-bwme2kFVw&s" alt="img" /><br />
+                <button onClick={almay}>Chek ON</button>
+        </div>
+
+        </>
+    )
+}
+export default FristPro;
